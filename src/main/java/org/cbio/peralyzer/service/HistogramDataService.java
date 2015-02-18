e800243a6d56b13f2d89997f17806ac63840c512
Onur Sumer <s.onur.sumer@gmail.com>
2015-02-18 17:09:27 -0500

    fixed histogram data filename problem

--- src/main/java/org/cbio/peralyzer/service/HistogramDataService.java
+++ src/main/java/org/cbio/peralyzer/service/HistogramDataService.java
@@ -4,6 +4,7 @@ import org.springframework.cache.annotation.Cacheable;
 import org.springframework.core.io.Resource;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
@@ -24,10 +25,7 @@ public class HistogramDataService
 	@Cacheable("histogramDataCache")
 	public String getHistogramData(String prediction) throws IOException
 	{
-		String filename = this.getHistogramDataResource().getFile().getAbsolutePath() +
-		                  "/" + prediction + ".json";
-
-		BufferedReader in = new BufferedReader(new FileReader(filename));
+		BufferedReader in = new BufferedReader(new FileReader(generateFilename(prediction)));
 		String line;
 		StringBuilder buffer = new StringBuilder();
 
@@ -40,6 +38,33 @@ public class HistogramDataService
 		in.close();
 
 		return buffer.toString();
+	}
+
+	protected String generateFilename(String prediction) throws IOException
+	{
+		String filename = this.getHistogramDataResource().getFile().getAbsolutePath() +
+		                  "/" + prediction + ".json";
+
+		File histFile = new File(filename);
+
+		// swap the node names and try again
+		if (!histFile.exists())
+		{
+			String parts[] = prediction.split("_");
+
+			if (parts.length > 4)
+			{
+				String newPredict = "predict_" + parts[2] + "_" +
+				                    parts[1] + "_" +
+			                        parts[3] + "_" +
+			                        parts[4];
+
+				filename = this.getHistogramDataResource().getFile().getAbsolutePath() +
+				           "/" + newPredict + ".json";
+			}
+		}
+
+		return filename;
 	}
 }
 
